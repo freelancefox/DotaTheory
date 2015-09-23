fa230539d663148eb5df063a1c62d6d0a1a0cb55
Jon Burnett <jburnett96@gmail.com>
2015-09-22 23:12:49 -0400

    Update to v1.0--getting the site online

---
+++ js/charts.js
@@ -0,0 +1,463 @@
+$(function () {
+  $('#stats1').highcharts({
+
+    chart: {
+      type: 'column',
+      backgroundColor:'rgba(255,255,255,0.175)'
+    },
+
+
+    title: {
+      text: 'Total Base Stats',
+      style: {
+        margin:'8px',
+        color:'#1D3E37',
+        fontSize:'15px',
+        fontWeight:'bold'
+      }
+    },
+
+    xAxis: {
+      categories: [
+        'Phantom Lancer',
+        'Outworld Devourer',
+        'Bane',
+        'Meepo',
+        'Bloodseeker',
+        'Leshrac',
+        '<b>Winter Wyvern</b>',
+        'Riki'],
+      labels: {
+        style: {
+          fontSize:'0.8em'
+        }
+      }
+    },
+
+    yAxis: {
+      allowDecimals: false,
+      min: 0,
+      tickInterval: 10,
+      title: {
+        text: null
+      },
+      stackLabels: {
+        enabled: true,
+        style: {
+          fontSize:'0.75em',
+          color:'#1D3E37'
+        }
+      }
+    },
+
+    tooltip: {
+      headerFormat: '<b>{point.x}</b><br/>',
+      pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+    },
+
+    credits: {
+      enabled: false
+    },
+
+    legend: {
+      enabled: false
+    },
+
+    plotOptions: {
+      column: {
+        stacking: 'normal',
+        dataLabels: {
+          enabled: true,
+          color:'#1D3E37'
+        }
+      }
+    },
+
+    series: [{
+      name: 'Intelligence',
+      data: [21, 26, 22, 20, 18, 26, 25, 14]
+    }, {
+      name: 'Strength',
+      data: [21, 19, 22, 23, 23, 16, 24, 17]
+    }, {
+      name: 'Agility',
+      data: [29, 24, 22, 23, 24, 23, 16, 34]
+    }]
+  });
+});
+$(function () {
+  $('#stats2').highcharts({
+
+    chart: {
+      type: 'column',
+      backgroundColor:'rgba(255,255,255,0.175)'
+    },
+
+    title: {
+      text: 'Total Stat Growths',
+      style: {
+        margin:'8px',
+        color:'#1D3E37',
+        fontSize:'15px',
+        fontWeight:'bold'
+      }
+    },
+
+    xAxis: {
+      categories: [
+        'Silencer',
+        'Centaur Warrunner',
+        'Ogre Magi',
+        'Outworld Devourer',
+        'Vengeful Spirit',
+        'Treant Protector',
+        '<b>Winter Wyvern</b>',
+        'Naga Siren'],
+      labels: {
+        style: {
+          fontSize:'0.8em'
+        }
+      }
+    },
+
+    yAxis: {
+      allowDecimals: false,
+      min: 0,
+      tickInterval: 5,
+      title: {
+        text: null
+      },
+      stackLabels: {
+        enabled: true,
+        style: {
+          fontSize:'0.75em',
+          color:'#1D3E37'
+        }
+      }
+    },
+
+    tooltip: {
+      headerFormat: '<b>{point.x}</b><br/>',
+      pointFormat: '{series.name} Gain: {point.y}<br/>Total: {point.stackTotal}'
+    },
+
+    credits: {
+      enabled: false
+    },
+
+    legend: {
+      enabled: false
+    },
+
+    plotOptions: {
+      column: {
+        stacking: 'normal',
+        dataLabels: {
+          enabled: true,
+          color:'#1D3E37'
+        }
+      }
+    },
+
+    series: [{
+      name: 'Intelligence',
+      data: [2.5, 1.6, 2.4, 3.3, 1.75, 1.8, 3.1, 1.95]
+    }, {
+      name: 'Strength',
+      data: [2.2, 3.8, 3.2, 1.85, 2.6, 3.3, 2.1, 2.3]
+    }, {
+      name: 'Agility',
+      data: [3, 2, 1.55, 2, 2.8, 2, 1.9, 2.75]
+    }]
+  });
+});
+$(function () {
+  $('#blast').highcharts({
+
+    chart: {
+      type: 'column',
+      backgroundColor:'rgba(255,255,255,0.175)'
+    },
+
+    title: {
+      text: 'Top AoE Nukes by DPS',
+      style: {
+        margin:'8px',
+        color:'#1D3E37',
+        fontSize:'15px',
+        fontWeight:'bold'
+      }
+    },
+
+    legend: {
+      enabled: false
+    },
+
+    xAxis: {
+      categories: [
+        'Arc Lightning',
+        'Crypt Swarm',
+        'Static Remnant',
+        'Lightning Storm',
+        'Nether Blast',
+        'Timber Chain',
+        'Illuminate',
+        'Dark Pact',
+        '<b>Splinter Blast</b>',
+        'Scream of Pain',
+        'Shockwave',
+        'Ether Shock',
+        'Powershot',
+        'Earthshock'],
+      useHTML: 'true',
+      labels: {
+        style: {
+          fontSize:'0.85em'
+        }
+      }
+    },
+
+    yAxis: {
+      tickInterval: '10',
+      max: '110',
+      style: {
+        fontSize:'0.75em',
+        color:'#1D3E37'
+      },
+      title: {
+        text: null
+      }
+    },
+
+    tooltip: {
+      formatter: function () {
+        return '<b>' + this.x + '</b><br/>' +
+          this.series.name + ': ' + this.y;
+      }
+    },
+
+    credits: {
+      enabled: false
+    },
+
+    plotOptions: {
+      column: {
+        stacking: 'normal'
+      }
+    },
+
+    series: [{
+      name: 'DPS',
+      type: 'column',
+      data: [109.3, 75, 74.3, 65, 59.1, 55, 50, 50, 48.6, 42.9, 42.9, 40, 40, 40]
+    }]
+  });
+});
+$(function () {
+  $('#curse').highcharts({
+
+    chart: {
+      type: 'column',
+      backgroundColor:'rgba(255,255,255,0.175)'
+    },
+
+    title: {
+      text: 'BKB-piercing Disable Ranges',
+      style: {
+        margin:'8px',
+        color:'#1D3E37',
+        fontSize:'15px',
+        fontWeight:'bold'
+      }
+    },
+
+    legend: {
+      enabled: false
+    },
+
+    xAxis: {
+      categories: [
+        'Hookshot',
+        'Chaotic Offering',
+        'Supernova',
+        '<b>Winter\'s Curse</b>',
+        'Nether Strike',
+        'Fiend\'s Grip',
+        'Primal Roar',
+        'Reverse Polarity',
+        'Berserker\'s Call'],
+      useHTML: 'true',
+      labels: {
+        style: {
+          fontSize:'0.9em'
+        }
+      }
+
+    },
+
+    yAxis: {
+      tickInterval: '500',
+      max: '3000',
+      style: {
+        fontSize:'0.75em',
+        color:'#1D3E37'
+      },
+      title: {
+        text: null
+      }
+    },
+
+    tooltip: {
+      formatter: function () {
+        return '<b>' + this.x + '</b><br/>' +
+          this.series.name + ': ' + this.y;
+      }
+    },
+
+    credits: {
+      enabled: false
+    },
+
+    plotOptions: {
+      column: {
+        stacking: 'normal'
+      }
+    },
+
+    series: [{
+      name: 'Effective Range',
+      data: [3000, 1800, 1000, 800, 700, 625, 600, 410, 300],
+    }]
+  });
+});
+$(function () {
+  $('#current').highcharts({
+
+    chart: {
+      type: 'pie',
+      backgroundColor:'rgba(255,255,255,0.175)'
+    },
+
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true,
+          format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+          style: {
+            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+          }
+        }
+      }
+    },
+
+    title: {
+      text: 'TI5',
+      style: {
+        margin:'4px',
+        color:'#1D3E37',
+        fontSize:'15px',
+        fontWeight:'bold'
+      }
+    },
+
+    tooltip: {
+      formatter: function () {
+        return this.series.name + ': ' + this.y;
+      }
+    },
+
+    credits: {
+      enabled: false
+    },
+
+    plotOptions: {
+      column: {
+        stackipredictee: 'normal'
+      }
+    },
+
+    series: [{
+      name: "Pick Rate",
+      colorByPoint: true,
+      data: [{
+        name: "4",
+        y: 0.58
+      }, {
+        name: "5",
+        y: 0.42
+      }]
+    }]
+  });
+});
+$(function () {
+  $('#predicted').highcharts({
+
+    chart: {
+      type: 'pie',
+      backgroundColor:'rgba(255,255,255,0.175)'
+    },
+
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true,
+          format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+          style: {
+            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+          }
+        }
+      }
+    },
+
+    title: {
+      text: 'Predicted',
+      style: {
+        margin:'4px',
+        color:'#1D3E37',
+        fontSize:'15px',
+        fontWeight:'bold'
+      }
+    },
+
+    tooltip: {
+      formatter: function () {
+        return this.series.name + ': ' + this.y;
+      }
+    },
+
+    credits: {
+      enabled: false
+    },
+
+    plotOptions: {
+      column: {
+        stacking: 'normal'
+      }
+    },
+
+    series: [{
+      name: "Pick Rate",
+      colorByPoint: true,
+      data: [{
+        name: "5",
+        y: 0.35
+      }, {
+        name: "4",
+        y: 0.22
+      }, {
+        name: "3",
+        y: 0.17
+      }, {
+        name: "2",
+        y: 0.25
+      }, {
+        name: "1",
+        y: 0.01
+      }]
+    }]
+  });
+});
---
+++ js/popovers.js
@@ -0,0 +1,662 @@
+jQuery(document).ready(function() {
+   $('#popover-soulring').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'left',
+      html: true,
+      content: function() {
+        return $('#content-soulring').html();
+      }
+  });
+   $('#popover-boots').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'left',
+      html: true,
+      content: function() {
+        return $('#content-boots').html();
+      }
+  });
+   $('#popover-tango').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'left',
+      html: true,
+      content: function() {
+        return $('#content-tango').html();
+      }
+  });
+   $('#popover-nulltalisman').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'right',
+      html: true,
+      content: function() {
+        return $('#content-nulltalisman').html();
+      }
+  });
+   $('#popover-sagesmask').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-sagesmask').html();
+      }
+  });
+   $('#popover-oov').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-oov').html();
+      }
+  });
+   $('#popover-treads').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'bottom',
+      html: true,
+      content: function() {
+        return $('#content-treads').html();
+      }
+  });
+   $('#popover-phase').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'bottom',
+      html: true,
+      content: function() {
+        return $('#content-phase').html();
+      }
+  });
+   $('#popover-necro1').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'right',
+      html: true,
+      content: function() {
+        return $('#content-necro1').html();
+      }
+  });
+   $('#popover-necro2').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'right',
+      html: true,
+      content: function() {
+        return $('#content-necro2').html();
+      }
+  });
+   $('#popover-necro3').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'right',
+      html: true,
+      content: function() {
+        return $('#content-necro3').html();
+      }
+  });
+   $('#popover-maelstrom').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'left',
+      html: true,
+      content: function() {
+        return $('#content-maelstrom').html();
+      }
+  });
+   $('#popover-mom').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'left',
+      html: true,
+      content: function() {
+        return $('#content-mom').html();
+      }
+  });
+   $('#popover-bots').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-bots').html();
+      }
+  });
+   $('#popover-orchid').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-orchid').html();
+      }
+  });
+   $('#popover-necrobook').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-necrobook').html();
+      }
+  });
+   $('#popover-refresher').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-refresher').html();
+      }
+  });
+   $('#popover-blink').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-blink').html();
+      }
+  });
+   $('#popover-sheepstick').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-sheepstick').html();
+      }
+  });
+   $('#popover-bkb').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-bkb').html();
+      }
+  });
+   $('#popover-daedalus').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-daedalus').html();
+      }
+  });
+   $('#popover-mkb').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-mkb').html();
+      }
+  });
+   $('#popover-moonshard').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-moonshard').html();
+      }
+  });
+   $('#popover-assault').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-assault').html();
+      }
+  });
+   $('#popover-mjollnir').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-mjollnir').html();
+      }
+  });
+   $('#popover-skadi').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-skadi').html();
+      }
+  });
+   $('#popover-glimmer').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-glimmer').html();
+      }
+  });
+   $('#popover-force').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-force').html();
+      }
+  });
+   $('#popover-pipe').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-pipe').html();
+      }
+  });
+   $('#popover-veil').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-veil').html();
+      }
+  });
+   $('#popover-lotus').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-lotus').html();
+      }
+  });
+   $('#popover-linkens').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-linkens').html();
+      }
+  });
+   $('#popover-darkseer').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-darkseer').html();
+      }
+  });
+   $('#popover-phoenix').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-phoenix').html();
+      }
+  });
+   $('#popover-troll').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-troll').html();
+      }
+  });
+   $('#popover-drow').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-drow').html();
+      }
+  });
+   $('#popover-aa').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-aa').html();
+      }
+  });
+   $('#popover-oracle').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-oracle').html();
+      }
+  });
+   $('#popover-viper').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-viper').html();
+      }
+  });
+   $('#popover-od').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-od').html();
+      }
+  });
+   $('#popover-leshrac').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-leshrac').html();
+      }
+  });
+   $('#popover-storm').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-storm').html();
+      }
+  });
+   $('#popover-lina').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-lina').html();
+      }
+  });
+   $('#popover-silencer').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-silencer').html();
+      }
+  });
+   $('#popover-meepo').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-meepo').html();
+      }
+  });
+   $('#popover-sniper').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-sniper').html();
+      }
+  });
+   $('#popover-pa').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-pa').html();
+      }
+  });
+   $('#popover-sven').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-sven').html();
+      }
+  });
+   $('#popover-juggernaut').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-juggernaut').html();
+      }
+  });
+   $('#popover-shadowfiend').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-shadowfiend').html();
+      }
+  });
+   $('#popover-lvl1').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-lvl1').html();
+      }
+  });
+   $('#popover-lvl2').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-lvl2').html();
+      }
+  });
+   $('#popover-lvl3').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-lvl3').html();
+      }
+  });
+   $('#popover-lvl4').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-lvl4').html();
+      }
+  });
+   $('#popover-lvl5').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-lvl5').html();
+      }
+  });
+   $('#popover-lvl6').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-lvl6').html();
+      }
+  });
+   $('#popover-lvl7').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-lvl7').html();
+      }
+  });
+   $('#popover-lvl8').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-lvl8').html();
+      }
+  });
+   $('#popover-lvl9').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-lvl9').html();
+      }
+  });
+   $('#popover-lvl10').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-lvl10').html();
+      }
+  });
+   $('#popover-lvl11').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-lvl11').html();
+      }
+  });
+   $('#popover-lvl12').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-lvl12').html();
+      }
+  });
+   $('#popover-lvl13').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-lvl13').html();
+      }
+  });
+   $('#popover-lvl14').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-lvl14').html();
+      }
+  });
+   $('#popover-lvl15').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-lvl15').html();
+      }
+  });
+   $('#popover-lvl16').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top',
+      html: true,
+      content: function() {
+        return $('#content-lvl16').html();
+      }
+  });
+});

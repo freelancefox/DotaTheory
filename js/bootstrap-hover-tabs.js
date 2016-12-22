9629192d6bb8b431376d8e99ae9087574f8bb410
Jon Burnett <jburnett96@gmail.com>
2016-12-21 22:36:31 -0500

    Updates all around

--- js/bootstrap-hover-tabs.js
+++ js/bootstrap-hover-tabs.js
@@ -1,7 +1,7 @@
 (function ($) {
   $(function () {
     $(document).off('click.bs.tab.data-api', '[data-hover="tab"]');
-    $(document).on('mouseenter.bs.tab.data-api', '[data-toggle="tab"], [data-hover="tab"]', function () {
+    $(document).on('mouseenter.bs.tab.data-api', '[data-hover="tab"]', function () {
       $(this).tab('show');
     });
   });

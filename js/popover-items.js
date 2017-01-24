9bf28b611d712b8197d1630b9bb4f669dc73a36f
Jon Burnett <jburnett96@gmail.com>
2017-01-24 05:44:47 -0500

    Add WIP 7.02 items

---
+++ js/popover-items.js
@@ -0,0 +1,422 @@
+jQuery(document).ready(function() {
+   $('#echo').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-echo').html();
+      }
+  });
+   $('#dlance').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-dlance').html();
+      }
+  });
+   $('#force').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-force').html();
+      }
+  });
+   $('#gauntlets').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-gauntlets').html();
+      }
+  });
+   $('#mantle').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-mantle').html();
+      }
+  });
+   $('#slippers').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-slippers').html();
+      }
+  });
+   $('#belt').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-belt').html();
+      }
+  });
+   $('#robe').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-robe').html();
+      }
+  });
+   $('#band').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-band').html();
+      }
+  });
+   $('#club').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-staff').html();
+      }
+  });
+   $('#staff').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-staff').html();
+      }
+  });
+   $('#blade').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-blade').html();
+      }
+  });
+   $('#sange').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'left auto',
+      html: true,
+      content: function() {
+        return $('#content-sange').html();
+      }
+  });
+   $('#lucen').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-lucen').html();
+      }
+  });
+   $('#yasha').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'right auto',
+      html: true,
+      content: function() {
+        return $('#content-yasha').html();
+      }
+  });
+   $('#reaver').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-reaver').html();
+      }
+  });
+   $('#mystic').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-mystic').html();
+      }
+  });
+   $('#eagle').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-eagle').html();
+      }
+  });
+   $('#halberd').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'bottom auto',
+      html: true,
+      content: function() {
+        return $('#content-halberd').html();
+      }
+  });
+   $('#gjalla').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'bottom',
+      html: true,
+      content: function() {
+        return $('#content-gjalla').html();
+      }
+  });
+   $('#spellblade').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'bottom',
+      html: true,
+      content: function() {
+        return $('#content-spellblade').html();
+      }
+  });
+   $('#seraph').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'bottom auto',
+      html: true,
+      content: function() {
+        return $('#content-seraph').html();
+      }
+  });
+   $('#manta').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'bottom auto',
+      html: true,
+      content: function() {
+        return $('#content-manta').html();
+      }
+  });
+   $('#hayabusa').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'bottom auto',
+      html: true,
+      content: function() {
+        return $('#content-hayabusa').html();
+      }
+  });
+   $('#sny').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'left auto',
+      html: true,
+      content: function() {
+        return $('#content-sny').html();
+      }
+  });
+   $('#snl').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'bottom auto',
+      html: true,
+      content: function() {
+        return $('#content-snl').html();
+      }
+  });
+   $('#lny').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'right auto',
+      html: true,
+      content: function() {
+        return $('#content-lny').html();
+      }
+  });
+   $('#trio').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'bottom auto',
+      html: true,
+      content: function() {
+        return $('#content-trio').html();
+      }
+  });
+   $('#heart').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-heart').html();
+      }
+  });
+   $('#satanic').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-satanic').html();
+      }
+  });
+   $('#pauldron').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-pauldron').html();
+      }
+  });
+   $('#sheep').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-sheep').html();
+      }
+  });
+   $('#shiva').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-shiva').html();
+      }
+  });
+   $('#ocore').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-ocore').html();
+      }
+  });
+   $('#butterfly').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-butterfly').html();
+      }
+  });
+   $('#eblade').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-eblade').html();
+      }
+  });
+   $('#wkb').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-wkb').html();
+      }
+  });
+   $('#wyrmslayer').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-wyrmslayer').html();
+      }
+  });
+   $('#pike').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'top auto',
+      html: true,
+      content: function() {
+        return $('#content-pike').html();
+      }
+  });
+   $('#urn').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'bottom auto',
+      html: true,
+      content: function() {
+        return $('#content-urn').html();
+      }
+  });
+   $('#frost').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'bottom auto',
+      html: true,
+      content: function() {
+        return $('#content-frost').html();
+      }
+  });
+   $('#pms').popover({
+      toggle:'popover',
+      container:'body',
+      trigger:'hover click',
+      placement:'bottom auto',
+      html: true,
+      content: function() {
+        return $('#content-pms').html();
+      }
+  });
+});
---
+++ js/svgDraw.js
@@ -0,0 +1,345 @@
+//helper functions, it turned out chrome doesn't support Math.sgn() 
+function signum(x) {
+    return (x < 0) ? -1 : 1;
+}
+function absolute(x) {
+    return (x < 0) ? -x : x;
+}
+
+function drawPath(svg, path, startX, startY, endX, endY) {
+    // get the path's stroke width (if one wanted to be  really precize, one could use half the stroke size)
+    var stroke =  parseFloat(path.attr("stroke-width"));
+    // check if the svg is big enough to draw the path, if not, set heigh/width
+    if (svg.attr("height") <  endY)                 svg.attr("height", endY);
+    if (svg.attr("width" ) < (startX + stroke) )    svg.attr("width", (startX + stroke));
+    if (svg.attr("width" ) < (endX   + stroke) )    svg.attr("width", (endX   + stroke));
+
+    var deltaX = (endX - startX) * 0.15;
+    var deltaY = (endY - startY) * 0.15;
+    // for further calculations which ever is the shortest distance
+    var delta  =  deltaY < absolute(deltaX) ? deltaY : absolute(deltaX);
+
+    // set sweep-flag (counter/clock-wise)
+    // if start element is closer to the left edge,
+    // draw the first arc counter-clockwise, and the second one clock-wise
+    var arc1 = 0; var arc2 = 1;
+    if (startX > endX) {
+        arc1 = 1;
+        arc2 = 0;
+    }
+    // draw tha pipe-like path
+    // 1. move a bit down, 2. arch,  3. move a bit to the right, 4.arch, 5. move down to the end
+    path.attr("d",  "M"  + startX + " " + startY +
+                    " V" + (startY + delta) +
+                    " A" + delta + " " +  delta + " 0 0 " + arc1 + " " + (startX + delta*signum(deltaX)) + " " + (startY + 2*delta) +
+                    " H" + (endX - delta*signum(deltaX)) + 
+                    " A" + delta + " " +  delta + " 0 0 " + arc2 + " " + endX + " " + (startY + 3*delta) +
+                    " V" + endY );
+}
+
+function connectElements(svg, path, startElem, endElem) {
+    var svgContainer= $("#svgContainer");
+
+    // if first element is lower than the second, swap!
+    if(startElem.offset().top > endElem.offset().top){
+        var temp = startElem;
+        startElem = endElem;
+        endElem = temp;
+    }
+
+    // get (top, left) corner coordinates of the svg container
+    var svgTop  = svgContainer.offset().top;
+    var svgLeft = svgContainer.offset().left;
+
+    // get (top, left) coordinates for the two elements
+    var startCoord = startElem.offset();
+    var endCoord   = endElem.offset();
+
+    // calculate path's start (x,y)  coords
+    // we want the x coordinate to visually result in the element's mid point
+    var startX = startCoord.left + 0.5*startElem.outerWidth() - svgLeft;    // x = left offset + 0.5*width - svg's left offset
+    var startY = startCoord.top  + startElem.outerHeight() - svgTop;        // y = top offset + height - svg's top offset
+
+        // calculate path's end (x,y) coords
+    var endX = endCoord.left + 0.5*endElem.outerWidth() - svgLeft;
+    var endY = endCoord.top  - svgTop;
+
+    // call function for drawing the path
+    drawPath(svg, path, startX, startY, endX, endY);
+
+}
+
+function connectAll() {
+    connectElements($("#svg"), $("#path1"), $("#belt"),  $("#sange"));
+    connectElements($("#svg"), $("#path2"), $("#robe"),  $("#lucen"));
+    connectElements($("#svg"), $("#path3"), $("#band"),  $("#yasha"));
+    connectElements($("#svg"), $("#path4"), $("#club"),  $("#sange"));
+    connectElements($("#svg"), $("#path5"), $("#staff"), $("#lucen"));
+    connectElements($("#svg"), $("#path6"), $("#blade"), $("#yasha"));
+    connectElements($("#svg"), $("#path7"), $("#gauntlets"),  $("#urn"));
+    connectElements($("#svg"), $("#path8"), $("#mantle"),  $("#frost"));
+    connectElements($("#svg"), $("#path9"), $("#slippers"),  $("#pms"));
+    connectElements($("#svg"), $("#path10"), $("#sange"),  $("#halberd"));
+    connectElements($("#svg"), $("#path11"), $("#sange"),  $("#gjalla"));
+    connectElements($("#svg"), $("#path12"), $("#sange"),  $("#sny"));
+    connectElements($("#svg"), $("#path13"), $("#sange"),  $("#snl"));
+    connectElements($("#svg"), $("#path14"), $("#sange"),  $("#trio"));
+    connectElements($("#svg"), $("#path15"), $("#lucen"),  $("#spellblade"));
+    connectElements($("#svg"), $("#path16"), $("#lucen"),  $("#seraph"));
+    connectElements($("#svg"), $("#path17"), $("#lucen"),  $("#snl"));
+    connectElements($("#svg"), $("#path18"), $("#lucen"),  $("#lny"));
+    connectElements($("#svg"), $("#path19"), $("#lucen"),  $("#trio"));
+    connectElements($("#svg"), $("#path20"), $("#yasha"),  $("#manta"));
+    connectElements($("#svg"), $("#path21"), $("#yasha"),  $("#hayabusa"));
+    connectElements($("#svg"), $("#path22"), $("#yasha"),  $("#sny"));
+    connectElements($("#svg"), $("#path23"), $("#yasha"),  $("#lny"));
+    connectElements($("#svg"), $("#path24"), $("#yasha"),  $("#trio"));
+    connectElements($("#svg"), $("#path25"), $("#force"),  $("#pike"));
+    connectElements($("#svg"), $("#path26"), $("#dlance"),  $("#pike"));
+    connectElements($("#svg"), $("#path27"), $("#dlance"),  $("#wyrmslayer"));
+    connectElements($("#svg"), $("#path28"), $("#echo"),  $("#wyrmslayer"));
+    connectElements($("#svg"), $("#path29"), $("#sny"),  $("#trio"));
+    connectElements($("#svg"), $("#path30"), $("#snl"),  $("#trio"));
+    connectElements($("#svg"), $("#path31"), $("#lny"),  $("#trio"));
+    connectElements($("#svg"), $("#path32"), $("#reaver"),  $("#heart"));
+    connectElements($("#svg"), $("#path33"), $("#reaver"),  $("#satanic"));
+    connectElements($("#svg"), $("#path34"), $("#reaver"),  $("#pauldron"));
+    connectElements($("#svg"), $("#path35"), $("#mystic"),  $("#sheep"));
+    connectElements($("#svg"), $("#path36"), $("#mystic"),  $("#shiva"));
+    connectElements($("#svg"), $("#path37"), $("#mystic"),  $("#ocore"));
+    connectElements($("#svg"), $("#path38"), $("#eagle"),  $("#butterfly"));
+    connectElements($("#svg"), $("#path39"), $("#eagle"),  $("#eblade"));
+    connectElements($("#svg"), $("#path40"), $("#eagle"),  $("#wkb"));
+    connectElements($("#svg"), $("#path41"), $("#force"),  $("#seraph"));
+    //connectElements($("#svg"), $("#path42"), $("#lny"),  $("#trio"));
+    //connectElements($("#svg"), $("#path43"), $("#lny"),  $("#trio"));
+}
+
+function hideAll() {
+    $("[id^=path]").hide();
+}
+
+$(document).ready(function() {
+    // reset svg each time
+    $("#svg").attr("height", "0");
+    $("#svg").attr("width", "0");
+    connectAll();
+    hideAll();
+});
+
+$("#belt, #club, #halberd, #gjalla").hover(function() {
+    $("#path1").show(150);
+    $("#path4").show(150);
+    $("#path10").show(150);
+    $("#path11").show(150);
+  }, function() {
+    $("#path1").hide();
+    $("#path4").hide();
+    $("#path10").hide();
+    $("#path11").hide();
+});
+
+$("#sange").hover(function() {
+    $("#path1").show(150);
+    $("#path4").show(150);
+    $("#path12").show(150);
+    $("#path13").show(150);
+    $("#path14").show(150);
+  }, function() {
+    $("#path1").hide();
+    $("#path4").hide();
+    $("#path12").hide();
+    $("#path13").hide();
+    $("#path14").hide();
+});
+
+$("#robe, #staff, #spellblade").hover(function() {
+    $("#path2").show(150);
+    $("#path5").show(150);
+    $("#path15").show(150);
+    $("#path16").show(150);
+  }, function() {
+    $("#path2").hide();
+    $("#path5").hide();
+    $("#path15").hide();
+    $("#path16").hide();
+});
+
+$("#seraph").hover(function() {
+    $("#path2").show(150);
+    $("#path5").show(150);
+    $("#path16").show(150);
+    $("#path41").show(150);
+  }, function() {
+    $("#path2").hide();
+    $("#path5").hide();
+    $("#path16").hide();
+    $("#path41").hide();
+});
+
+$("#lucen").hover(function() {
+    $("#path2").show(150);
+    $("#path5").show(150);
+    $("#path17").show(150);
+    $("#path18").show(150);
+    $("#path19").show(150);
+  }, function() {
+    $("#path2").hide();
+    $("#path5").hide();
+    $("#path17").hide();
+    $("#path18").hide();
+    $("#path19").hide();
+});
+
+$("#band, #blade, #manta, #hayabusa").hover(function() {
+    $("#path3").show(150);
+    $("#path6").show(150);
+    $("#path20").show(150);
+    $("#path21").show(150);
+  }, function() {
+    $("#path3").hide();
+    $("#path6").hide();
+    $("#path20").hide();
+    $("#path21").hide();
+});
+
+$("#yasha").hover(function() {
+    $("#path3").show(150);
+    $("#path6").show(150);
+    $("#path17").show(150);
+    $("#path18").show(150);
+    $("#path19").show(150);
+  }, function() {
+    $("#path3").hide();
+    $("#path6").hide();
+    $("#path17").hide();
+    $("#path18").hide();
+    $("#path19").hide();
+});
+
+$("#trio").hover(function() {
+    $("#path29").show(150);
+    $("#path30").show(150);
+    $("#path31").show(150);
+  }, function() {
+    $("#path29").hide();
+    $("#path30").hide();
+    $("#path31").hide();
+});
+
+$("#sny").hover(function() {
+    $("#path12").show(150);
+    $("#path22").show(150);
+  }, function() {
+    $("#path12").hide();
+    $("#path22").hide();
+});
+
+$("#snl").hover(function() {
+    $("#path13").show(150);
+    $("#path17").show(150);
+  }, function() {
+    $("#path13").hide();
+    $("#path17").hide();
+});
+
+$("#lny").hover(function() {
+    $("#path18").show(150);
+    $("#path23").show(150);
+  }, function() {
+    $("#path18").hide();
+    $("#path23").hide();
+});
+
+$("#gauntlets").hover(function() {
+    $("#path7").show(150);
+  }, function() {
+    $("#path7").hide();
+});
+
+$("#mantle").hover(function() {
+    $("#path8").show(150);
+  }, function() {
+    $("#path8").hide();
+});
+
+$("#slippers").hover(function() {
+    $("#path9").show(150);
+  }, function() {
+    $("#path9").hide();
+});
+
+$("#dlance").hover(function() {
+    $("#path26").show(150);
+    $("#path27").show(150);
+  }, function() {
+    $("#path26").hide();
+    $("#path27").hide();
+});
+
+$("#pike").hover(function() {
+    $("#path25").show(150);
+    $("#path26").show(150);
+  }, function() {
+    $("#path25").hide();
+    $("#path26").hide();
+});
+
+$("#echo").hover(function() {
+    $("#path28").show(150);
+  }, function() {
+    $("#path28").hide();
+});
+
+$("#force").hover(function() {
+    $("#path25").show(150);
+    $("#path41").show(150);
+  }, function() {
+    $("#path25").hide();
+    $("#path41").hide();
+});
+
+$("#wyrmslayer").hover(function() {
+    $("#path27").show(150);
+    $("#path28").show(150);
+  }, function() {
+    $("#path27").hide();
+    $("#path28").hide();
+});
+
+$("#reaver, #heart, #satanic, #pauldron").hover(function() {
+    $("#path32").show(150);
+    $("#path33").show(150);
+    $("#path34").show(150);
+  }, function() {
+    $("#path32").hide();
+    $("#path33").hide();
+    $("#path34").hide();
+});
+
+$("#mystic, #sheep, #shiva, #ocore").hover(function() {
+    $("#path35").show(150);
+    $("#path36").show(150);
+    $("#path37").show(150);
+  }, function() {
+    $("#path35").hide();
+    $("#path36").hide();
+    $("#path37").hide();
+});
+
+$("#eagle, #butterfly, #eblade, #wkb").hover(function() {
+    $("#path38").show(150);
+    $("#path39").show(150);
+    $("#path40").show(150);
+  }, function() {
+    $("#path38").hide();
+    $("#path39").hide();
+    $("#path40").hide();
+});
+
+$(window).resize(function () {
+    // reset svg each time
+    $("#svg").attr("height", "0");
+    $("#svg").attr("width", "0");
+    connectAll();
+});
